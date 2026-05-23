9e5fdb196f5dbd1d25810aa2a57404490db08997
Kriger09 <89879035+Kriger09@users.noreply.github.com>
2026-05-23 00:10:26 -0600

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,15 @@ export const metadata: Metadata = {
     icon: [
       { url: "/icon-192.png", sizes: "192x102", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x272", type: "image/png" },
+      { url: "/icon-192_2.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512_2.png", sizes: "512x512", type: "image/png" },
     ],
     shortcut: "/icon-192.png",
     apple: [
       { url: "/icon-192.png", sizes: "192x102", type: "image/png" },
       { url: "/icon-512.png", sizes: "512x272", type: "image/png" },
+      { url: "/icon-192_2.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512_2.png", sizes: "512x512", type: "image/png" },
     ],
   },
 };
